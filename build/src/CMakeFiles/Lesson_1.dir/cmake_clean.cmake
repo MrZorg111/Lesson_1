ed5211a9010a9de2e4f19eeea545e5fb2f12d355
MrZorg111 <yodji333@mail.ru>
2024-08-02 16:42:16 +0300

    first commit

---
+++ build/src/CMakeFiles/Lesson_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson_1.dir/main.cpp.obj"
+  "CMakeFiles/Lesson_1.dir/main.cpp.obj.d"
+  "Lesson_1.exe"
+  "Lesson_1.exe.manifest"
+  "Lesson_1.pdb"
+  "libLesson_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lesson_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
